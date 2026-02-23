4c486d0a77de25642bf0bc6289328059dff004be
irempirinc <irempirinc6@gmail.com>
2026-02-23 18:03:26 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+ // İsmin üzerine tıklandığında bir uyarı mesajı çıksın
+    const nameHeader = document.querySelector('h1');
+    nameHeader.onclick = function() {
+        alert("Bana ulaştığınız için teşekkürler! ✨");
+    };
